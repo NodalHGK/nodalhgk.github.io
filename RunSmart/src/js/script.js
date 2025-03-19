eba5a421f16f6c35f4139c1ac008e0bb7e18ceee
NodalHGK <oooo_84@inbox.ru>
2025-03-19 13:08:34 +0300

    add carousel

---
+++ RunSmart/src/js/script.js
@@ -0,0 +1,54 @@
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: true,
+  autoplayButtonOutput: false,
+  speed: 3000,
+  controls: false,
+  navPosition: "bottom",
+  responsive: {
+    640: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 2
+    },
+    700: {
+      gutter: 30
+    },
+    998: {
+      items: 1,
+      nav: true,
+    },
+  }
+});
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         dots: false,
+//         speed: 1200,
+//         slidesToShow: 1,
+//         adaptiveHeight: false,
+//         autoplay: true,
+//         autoplaySpeed: 3000,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/left-arrow.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/right-arrow.png"></button>',
+//         responsive: [
+//           {
+//             breakpoint: 992,
+//             settings: {
+//               dots: true,
+//               arrows: false,
+//             }
+//           }
+//         ],
+
+    
+//       });
+// });
+
