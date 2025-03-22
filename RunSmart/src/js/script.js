f6df19970fc7d2742682f9c609e72ad8bcb20204
NodalHGK <oooo_84@inbox.ru>
2025-03-22 17:25:30 +0300

    modified js file

--- RunSmart/src/js/script.js
+++ RunSmart/src/js/script.js
@@ -22,27 +22,5 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
 });
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         dots: false,
-//         speed: 1200,
-//         slidesToShow: 1,
-//         adaptiveHeight: false,
-//         autoplay: true,
-//         autoplaySpeed: 3000,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/left-arrow.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/right-arrow.png"></button>',
-//         responsive: [
-//           {
-//             breakpoint: 992,
-//             settings: {
-//               dots: true,
-//               arrows: false,
-//             }
-//           }
-//         ],
 
-    
-//       });
-// });
 
