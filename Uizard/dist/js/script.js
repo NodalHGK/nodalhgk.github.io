c43d85fe9643a98914678ea240543f2050d69173
NodalHGK <oooo_84@inbox.ru>
2025-04-04 15:14:57 +0300

    promo ready

---
+++ Uizard/dist/js/script.js
@@ -0,0 +1,23 @@
+const slider = tns({
+    container: '.promo__slider',
+    items: 6,
+    slideBy: '1',
+    autoplay: true,
+    loop: true,
+    autoplayButtonOutput: false,
+    autoplayTimeout: 0, 
+    speed: 11000,
+    controls: false,
+    nav: false,
+    rewind: false,
+    preventScrollOnTouch: 'force',
+    responsive: {
+      1051: {
+        
+      },
+      1050: {
+        
+      }
+    }
+  });
+  
